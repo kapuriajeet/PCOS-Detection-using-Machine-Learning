96daaef21c298e0cec9cad2e1ef6324bd2539664
jeetkapuria <jeetkapuria@gmail.com>
2022-03-09 17:52:19 +0530

    added two more Classifiers

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import streamlit as st
 
 # Loading the saved model
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model_lr = pickle.load(open('trained_model_lr.sav', 'rb'))
+loaded_model_xgb = pickle.load(open('trained_model_xgb.sav', 'rb'))
+loaded_model_cat = pickle.load(open('trained_model_cat.sav', 'rb'))
 
 # creating a function for prediction
+
+clf_name = st.sidebar.selectbox("Select Classifier Model: ",
+                                ("Logistic Regression", "XGRBF Classifier", "Catboost Classifier"))
 
 
 def pcos_prediction(input_data):
@@ -15,20 +20,36 @@ def pcos_prediction(input_data):
 
     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
-    prediction = loaded_model.predict(input_data_reshaped)
-    # cat_clf.predict(input_data_reshaped)
-    print(prediction)
-    if prediction == 0:
-        return "NO PCOS"
-    else:
-        return "PCOS"
+    if clf_name == 'Logistic Regression':
+        prediction = loaded_model_lr.predict(input_data_reshaped)
+        if prediction == 0:
+            return "NO PCOS"
+        else:
+            return "PCOS"
+    elif clf_name == 'XGRBF Classifier':
+        prediction = loaded_model_xgb.predict(input_data_reshaped)
+        if prediction == 0:
+            return "NO "
+        else:
+            return "PCOS"
+    elif clf_name == "Catboost Classifier":
+        prediction = loaded_model_cat.predict(input_data_reshaped)
+        if prediction == 0:
+            return "NO PCOS"
+        else:
+            return "PCOS"
 
 
 def main():
 
     # Title for web page
-    st.title('PCOS Prediction using Machine Learning Web App')
-
+    st.title('PCOS Prediction using Machine Learning')
+    if clf_name == 'Logistic Regression':
+        st.write("Logistic Regression")
+    elif clf_name == 'XGRBF Classifier':
+        st.write("XGRBF Classifier")
+    elif clf_name == 'Catboost Classifier':
+        st.write("Catboost Classifier")
     # Getting inputs from the user
     age = st.number_input('Please Mention your Age')
     stresslevels = st.number_input(
@@ -40,7 +61,6 @@ def main():
         'Do you Smoke? (If yes mention 1 and if No mention 0) :')
     alcohol = st.number_input(
         'Do you drink Alcohol? (If yes mention 1 and if No mention 0) :')
-    timebetweenperiods = st.number_input("Time between your periods: ")
     delayInPeriods = st.number_input("Delay in Periods (In Days): ")
     emotionalStatus = st.number_input("Emotional Status (answer 1 or 0): ")
     MalePattern_you = st.number_input(
@@ -59,7 +79,6 @@ def main():
                                      sleeprating,
                                      smoking,
                                      alcohol,
-                                     timebetweenperiods,
                                      delayInPeriods,
                                      emotionalStatus,
                                      MalePattern_you,
